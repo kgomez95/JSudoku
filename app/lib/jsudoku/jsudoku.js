4fc1c4c6c75fc88d8c4caa210debea373b018c76
kgomez <kevin.gomez.codina@outlook.es>
2021-10-23 22:26:43 +0200

    - Se ha creado la clase css para el sudoku. - Se ha creado el tablero en html. - Se han creado varias funciones para inicializar la partida. - Falta realizar un TODO importante acerca de cómo se generan los valores del tablero.

--- app/lib/jsudoku/jsudoku.js
+++ app/lib/jsudoku/jsudoku.js
@@ -2,38 +2,145 @@
  * JSudoku v0.1
  *
  * Autor: Kevin Gómez Codina
- * 
  * Date: 2021-10-23T10:00Z
  */
 "use strict";
 
 var JSudoku = (function () {
-    var _x = 9,
+    var _this = undefined,
+        _x = 9,
         _y = 9,
-        _rowValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        _rowValues = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        _domId = "";
 
     /**
      * @name JSudoku
      * @description Crea una partida para jugar a sudoku.
+     * @param domId - Identificador del contenedor div del DOM donde se mostrará el sudoku.
      */
-    function JSudoku() {
-        var _this = this;
+    function JSudoku(domId) {
+        // Variables privadas.
+        _this = this;
+        _domId = domId;
+
+        // Variables públicas.
         _this.board = new Array(_x);
 
         // Inicializamos el tablero.
-        _this.initBoard();
+        initBoard();
 
         return _this;
+    }
+
+    /**
+     * @name newGame
+     * @description Inicializa el juego.
+     */
+    JSudoku.prototype.newGame = function () {
+        var mainElement = getMainDOM(true);
+        var container = document.createElement("div");
+        var span = document.createElement("span");
+        var easyButton = document.createElement("button");
+        var normalButton = document.createElement("button");
+        var hardButton = document.createElement("button");
+
+        span.appendChild(document.createTextNode("Selecciona la dificultad:"));
+        easyButton.appendChild(document.createTextNode("Fácil"));
+        normalButton.appendChild(document.createTextNode("Normal"));
+        hardButton.appendChild(document.createTextNode("Difícil"));
+
+        easyButton.onclick = startEasyGame;
+        normalButton.onclick = startNormalGame;
+        hardButton.onclick = startHardGame;
+
+        container.appendChild(span);
+        container.appendChild(easyButton);
+        container.appendChild(normalButton);
+        container.appendChild(hardButton);
+
+        mainElement.appendChild(container);
+    };
+
+    /**
+     * @name startEasyGame
+     * @description Inicializa una partida en modo fácil.
+     */
+    function startEasyGame() {
+        var board = copyBoard(3);
+
+        initBoard();
+        createBoardView(board);
+    }
+
+    /**
+     * @name startNormalGame
+     * @description Inicializa una partida en modo normal.
+     */
+    function startNormalGame() {
+        var board = copyBoard(4);
+
+        initBoard();
+        createBoardView(board);
+    }
+
+    /**
+     * @name startHardGame
+     * @description Inicializa una partida en modo normal.
+     */
+    function startHardGame() {
+        var board = copyBoard(6);
+
+        initBoard();
+        createBoardView(board);
+    }
+
+    /**
+     * @name getMainDOM
+     * @param empty - Vacía el elemento antes de devolverlo.
+     * @returns Retorna el elemento contenedor DOM de JSudoku.
+     */
+    function getMainDOM(empty) {
+        var mainElement = document.getElementById(_domId);
+        mainElement.classList.add("jsudoku-board");
+        if (empty) mainElement.innerHTML = "";
+        return mainElement;
+    }
+
+    /**
+     * @name copyBoard
+     * @description Hace una copia del tablero y le quita tantos elementos a la fila como se indique en el parámetro "exclude".
+     * @param exclude - Indica el total de elementos a excluir en la fila.
+     * @returns Retorna la copia del tablero modificada.
+     */
+    function copyBoard(exclude) {
+        var board = _this.board.slice();
+
+        if (exclude < 0) exclude = 0;
+        else if (exclude > _y) exclude = _y;
+
+        for (var x = 0; x < _x; x++) {
+            var random = new Array(exclude);
+
+            for (var i = 0; i < exclude; i++) {
+                random.push(Math.floor(Math.random() * _y));
+            }
+
+            random.forEach(function (y) {
+                board[x][y] = "";
+            });
+        }
+
+        return board;
     }
 
     /**
      * @name initBoard
      * @description Inicializa los valores del tablero.
      */
-    JSudoku.prototype.initBoard = function () {
+    function initBoard() {
         // Inicializamos las posiciones del tablero.
         for (var i = 0; i < _x; i++) {
-            this.board[i] = new Array(_y);
+            _this.board[i] = new Array(_y);
         }
 
         // Inicializamos los valores del tablero.
@@ -43,7 +150,7 @@ var JSudoku = (function () {
             var rowValues = _rowValues.slice();
 
             // Inicializamos la fila del tablero en caso de que el valor no sea válido.
-            if (!isValid) this.board[x] = new Array(_y);
+            if (!isValid) _this.board[x] = new Array(_y);
 
             for (var y = 0; y < _y; (isValid) ? y++ : y = _y) {
                 var index = 0;
@@ -58,11 +165,16 @@ var JSudoku = (function () {
 
                     // Comprobamos si ya existe el valor en la columna actual (Y). No hace falta que comprobamos la fila (X), porque como estamos usando la lista "rowValues"
                     // sabemos con certeza que nunca que repetirán los valores en la fila, ya que cuando insertamos un valor lo eliminamos de la lista "rowValues".
-                    isValid = !this.board.some(function (xValue) {
+                    isValid = !_this.board.some(function (xValue) {
                         return xValue.some(function (yValue, i) {
                             return i === y && yValue == value;
                         });
                     });
+
+                    if (isValid) {
+                        // TODO: Hay que mejorar la validación. No puede haber el mismo número dentro del grupo de 3x3 celdas.
+
+                    }
 
                     // Aumentamos el contador de intentos.
                     attempts++;
@@ -74,11 +186,71 @@ var JSudoku = (function () {
                 if (isValid) {
                     // Si el valor a introducir es válido lo quitamos de la lista de valores disponibles y lo añadimos al tablero.
                     rowValues.splice(index, 1);
-                    this.board[x][y] = value;
+                    _this.board[x][y] = value;
                 }
             }
         }
-    };
+    }
+
+    /**
+     * @name createBoardView
+     * @description Crea el tablero en pantalla.
+     * @param board - Tablero a mostrar en pantalla.
+     */
+    function createBoardView(board) {
+        // Busca el elemento contenedor principal, le asigna la clase del tablero y lo vacía.
+        var mainElement = getMainDOM(true);
+
+        for (var x = 0; x < 3; x++) {
+            // Creamos la fila y le asignamos la clase.
+            var boardXElement = document.createElement("div");
+            boardXElement.classList.add("jsudoku-x");
+
+            for (var y = 0; y < 3; y++) {
+                // Creamos la columna y le asignamos la clase.
+                var boardYElement = document.createElement("div");
+                boardYElement.classList.add("jsudoku-y");
+
+                for (var i = 0; i < _rowValues.length; i++) {
+                    // Creamos las nueve celdas en la columna.
+                    var xPos = 0,
+                        yPos = 0,
+                        yCellElement = document.createElement("div");
+                    yCellElement.classList.add("jsudoku-y-cell");
+
+                    // Calculamos las posiciones para saber a qué posición del array pertenece cada celda.
+                    if (i < 3) {
+                        xPos = ((x * 2) + x);
+                        yPos = ((y * 3) + i);
+                    }
+                    else if (i < 6) {
+                        xPos = ((x * 3) + 1);
+                        yPos = ((y * 3) - 3 + i);
+                    }
+                    else {
+                        xPos = ((x * 3) + 2);
+                        yPos = ((y * 3) - 6 + i);
+                    }
+
+                    // Añadimos los atributos a la celda.
+                    yCellElement.setAttribute("x", xPos);
+                    yCellElement.setAttribute("y", yPos);
+
+                    // Mostramos el valor en la celda.
+                    yCellElement.appendChild(document.createTextNode(board[xPos][yPos]));
+
+                    // Añadimos la celda a la columna.
+                    boardYElement.appendChild(yCellElement);
+                }
+
+                // Añadimos la columna a la fila.
+                boardXElement.appendChild(boardYElement);
+            }
+
+            // Añadimos la fila al contenedor principal.
+            mainElement.appendChild(boardXElement);
+        }
+    }
 
     return JSudoku;
 })();
